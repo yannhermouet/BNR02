16095aecfab0bb06178d070e43212fe1e5568124
yannhermouet <yann.hermouet@mac.com>
2014-08-22 15:40:52 +0100

    Update03

---
+++ RandomItems/RandomItems/BNRItem.h
@@ -0,0 +1,13 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by Yann's AW Office on 22/08/2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+
+@end
---
+++ RandomItems/RandomItems/BNRItem.m
@@ -0,0 +1,13 @@
+//
+//  BNRItem.m
+//  RandomItems
+//
+//  Created by Yann's AW Office on 22/08/2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
+@end
